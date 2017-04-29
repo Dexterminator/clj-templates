7235d9bc9e964f0ebcae26740e53d2d65a1906a9
Dexter Gramfors <dexter.gramfors@gmail.com>
2017-04-29 12:03:17 +0200

    Add endpoint that returns all templates as transit

--- src/clj/clj_templates/handler.clj
+++ src/clj/clj_templates/handler.clj
@@ -1,18 +1,33 @@
 (ns clj-templates.handler
   (:require [compojure.core :refer [GET routes]]
             [compojure.route :refer [resources]]
-            [ring.util.response :refer [resource-response]]
+            [ring.util.response :refer [resource-response response]]
             [ring.logger.timbre :refer [wrap-with-logger]]
             [ring.middleware.defaults :refer [site-defaults wrap-defaults]]
             [ring.util.http-response :refer [content-type ok]]
-            [integrant.core :as ig]))
+            [clj-templates.db.db :as db]
+            [integrant.core :as ig]
+            [cognitect.transit :as transit])
+  (:import (java.io ByteArrayOutputStream)))
+
+(defn transit-json [v]
+  (let [out (ByteArrayOutputStream. 4096)
+        _ (-> out
+              (transit/writer :json)
+              (transit/write v))
+        res (.toString out)]
+    (.reset out)
+    res))
 
 (defn home-page []
-  (content-type (resource-response "index.html" {:root "public"}) "text/html; charset=utf-8"))
+  (-> (resource-response "index.html" {:root "public"})
+      (content-type "text/html; charset=utf-8")))
 
-;; TODO: Get from db
 (defn templates [db]
-  "Templates")
+  (let [all-templates {:templates (db/all-templates db)}
+        transit-templates (transit-json all-templates)]
+    (-> (response transit-templates)
+        (content-type "application/transit+json"))))
 
 (defn app-routes [db]
   (routes

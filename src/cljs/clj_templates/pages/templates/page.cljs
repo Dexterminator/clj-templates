bbef940bb011da04be75f142af2b80518395fd54
Dexter Gramfors <dexter.gramfors@gmail.com>
2017-05-03 07:51:10 +0200

    Add logos and basic descriptions to build system tabs.

--- src/cljs/clj_templates/pages/templates/page.cljs
+++ src/cljs/clj_templates/pages/templates/page.cljs
@@ -17,6 +17,20 @@
    [tab :lein "Leiningen" active-tab]
    [tab :boot "Boot" active-tab]])
 
+(def descriptions
+  {:lein {:text    "Leiningen is for automating Clojure projects without setting your hair on fire."
+          :image   "images/leiningen.jpg"
+          :command "lein new app"}
+   :boot {:text    "Builds are programs. Let's start treating them that way."
+          :image   "images/boot-logo.png"
+          :command "boot -d boot/new new -t app -n myapp"}})
+
+(defn build-system-info [active-tab]
+  (let [{:keys [image text command]} (descriptions active-tab)]
+    [:div.build-system-info
+     [:img.build-system-logo {:src image}]
+     [:div.build-system-text [:i text] [:pre command]]]))
+
 (defn search-input [templates]
   [:input.search-input {:type        "text"
                         :placeholder (str "Search " (count templates) " templates")}])
@@ -26,6 +40,7 @@
         active-tab (listen [:templates/active-tab])]
     [:div.templates
      [:h1 "Templates"]
+     [build-system-info active-tab]
      [tabs active-tab]
      [search-input templates]
      (for [{:keys [template-name build-system] :as template} templates]

cd6306dd7c968030714cf01a74c25d56c4332365
Dexter Gramfors <dexter.gramfors@gmail.com>
2017-05-09 00:20:51 +0200

    Update logger format config.

--- src/clj/clj_templates/logger.clj
+++ src/clj/clj_templates/logger.clj
@@ -1,9 +1,13 @@
 (ns clj-templates.logger
   (:require [integrant.core :as ig]
             [taoensso.timbre :as timbre]
-            [taoensso.timbre.appenders.core :as appenders]))
+            [taoensso.timbre.appenders.core :as appenders])
+  (:import (java.util Locale TimeZone)))
 
 (defmethod ig/init-key :logger/timbre [_ {:keys [appenders]}]
   (println (str "Initializing logging appenders: " (keys appenders)))
-  (timbre/merge-config! {:appenders {:println (when-let [opts (:println appenders)] (appenders/println-appender opts))
-                                     :spit    (when-let [opts (:spit appenders)] (appenders/spit-appender opts))}}))
+  (timbre/merge-config! {:appenders      {:println (when-let [opts (:println appenders)] (appenders/println-appender opts))
+                                          :spit    (when-let [opts (:spit appenders)] (appenders/spit-appender opts))}
+                         :timestamp-opts {:pattern  "yyyy-MM-dd HH:mm:ss"
+                                          :timezone (TimeZone/getDefault)
+                                          :locale   (Locale. "en")}}))

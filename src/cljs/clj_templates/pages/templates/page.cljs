86bec6f9631c0b1e032dc6d547f68ce7bee10889
Dexter Gramfors <dexter.gramfors@gmail.com>
2017-05-29 19:34:49 +0200

    Styling tweaks: use a 'card' look for the templates.

--- src/cljs/clj_templates/pages/templates/page.cljs
+++ src/cljs/clj_templates/pages/templates/page.cljs
@@ -3,15 +3,16 @@
             [clj-templates.util.js :refer [target-value]]
             [re-frame.core :refer [dispatch]]))
 
-(defn template-panel [{:keys [template-name description build-system homepage]}]
+(defn template-panel [{:keys [template-name description build-system homepage downloads]}]
   [:div.template
    [:div.title
     (if homepage
       [:a {:href homepage} template-name]
       template-name)]
    [:div description]
-   (when (= build-system "lein") [:div [:pre [:span.keyword ":lein "] "lein new " template-name " my-app"]])
-   [:div [:pre [:span.keyword ":boot "] "boot -d boot/new new -t " template-name "-n my-app"]]])
+   (when (= build-system "lein") [:div [:pre.keyword ":lein-usage "] [:pre.usage "lein new " template-name " my-app"]])
+   [:div [:pre.keyword ":boot-usage "] [:pre.usage "boot -d boot/new new -t " template-name "-n my-app"]]
+   [:div [:pre.keyword ":downloads"] [:pre.usage downloads]]])
 
 (defn search-input [templates]
   (let [template-count (count templates)]

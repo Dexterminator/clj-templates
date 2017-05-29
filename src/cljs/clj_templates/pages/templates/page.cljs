7b984171e6bc8fd6f6c59683b0e93dfe2946c5b5
Dexter Gramfors <dexter.gramfors@gmail.com>
2017-05-29 21:01:19 +0200

    Display templates in a grid, and increase size of search box.

--- src/cljs/clj_templates/pages/templates/page.cljs
+++ src/cljs/clj_templates/pages/templates/page.cljs
@@ -14,22 +14,16 @@
    [:div [:pre.keyword ":boot-usage "] [:pre.usage "boot -d boot/new new -t " template-name "-n my-app"]]
    [:div [:pre.keyword ":downloads"] [:pre.usage downloads]]])
 
-(defn search-input [templates]
-  (let [template-count (count templates)]
-    [:input.search-input {:type        "text"
-                          :placeholder (when (pos? template-count) (str "Search templates"))
-                          :on-change   #(dispatch [:templates/search (target-value %)])}]))
+(defn search-input []
+  [:input.search-input {:type        "text"
+                        :placeholder (str "Search templates")
+                        :on-change   #(dispatch [:templates/search (target-value %)])}])
 
 (defn templates []
   (let [templates (listen [:templates/templates])
         loading? (listen [:templates/loading?])]
     [:div.templates
-     [:div.template-title
-      [:h1 "Templates"]
-      (when loading?
-        [:div.spinner.templates-spinner])]
-     [search-input templates]
-     [:div
-
+     [search-input]
+     [:div.templates-listing
       (for [{:keys [template-name build-system] :as template} templates]
         ^{:key (str template-name build-system)} [template-panel template])]]))

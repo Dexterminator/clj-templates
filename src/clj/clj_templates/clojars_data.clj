678a8790cf4d29617e7396bbce30a26d5bf625ad
Dexter Gramfors <dexter.gramfors@gmail.com>
2017-09-12 21:48:25 +0200

    Use with-open to make sure gzip reader is properly closed.

--- src/clj/clj_templates/clojars_data.clj
+++ src/clj/clj_templates/clojars_data.clj
@@ -12,16 +12,17 @@
 
 (def edn-opts {:eof :eof})
 
-(defn gzip-seq [stream]
-  (let [in (-> stream
-               (GZIPInputStream.)
-               (InputStreamReader.)
-               (PushbackReader.))]
-    (take-while #(not= :eof %) (repeatedly #(edn/read edn-opts in)))))
+(defn read-gzip-edn [stream]
+  (with-open [in (-> stream
+                     (GZIPInputStream.)
+                     (InputStreamReader.)
+                     (PushbackReader.))]
+    (doall
+      (take-while #(not= :eof %) (repeatedly #(edn/read edn-opts in))))))
 
 (defn extract-templates-from-gzip-stream [stream]
   (filterv (fn [{:keys [artifact-id]}] (#{"lein-template" "boot-template"} artifact-id))
-           (gzip-seq stream)))
+           (read-gzip-edn stream)))
 
 (defn get-clojars-templates []
   (let [res @(http/get "http://clojars.org/repo/feed.clj.gz" {:as :stream})]

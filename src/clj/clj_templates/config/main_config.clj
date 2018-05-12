dc676efeae52758a47a3c5dabbe57bdcf84291f8
Dexter Gramfors <dexter.gramfors@gmail.com>
2018-05-12 12:45:03 +0200

    Parse heroku database_url into jdbc/hikaricp compatible url.

--- src/clj/clj_templates/config/main_config.clj
+++ src/clj/clj_templates/config/main_config.clj
@@ -1,12 +1,19 @@
 (ns clj-templates.config.main-config
   (:require [integrant.core :as ig]
-            [environ.core :refer [env]]))
+            [environ.core :refer [env]]
+            [clojure.string :as str])
+  (:import (java.net URI)))
+
+(defn get-jdbc-url []
+  (let [uri (URI. (env :database-url))
+        [name pwd] (str/split (.getUserInfo uri) #":")]
+    (str "jdbc:postgresql://" (.getHost uri) (.getPath uri) "?user=" name "&password=" pwd)))
 
 (def main-config
   {:handler/main        {:es-client (ig/ref :search/elastic)}
    :server/jetty        {:handler (ig/ref :handler/main)
                          :opts    {:join? false}}
-   :db/postgres         {:jdbc-url          (env :db-url)
+   :db/postgres         {:jdbc-url          (get-jdbc-url)
                          :driver-class-name "org.postgresql.Driver"}
    :logger/timbre       {:appenders {:println {:stream :auto}}}
    :jobs/scheduled-jobs {:hours-between-jobs 8

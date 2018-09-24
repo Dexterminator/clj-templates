abdda582a9c9dc673a53ea745fc0e69f49e73595
Dexter Gramfors <dexter.gramfors@gmail.com>
2018-09-24 23:02:38 +0200

    Change logo color and add links to clojure.org and clojurescript.org

--- src/cljs/clj_templates/components/header/component.cljs
+++ src/cljs/clj_templates/components/header/component.cljs
@@ -1,6 +1,12 @@
 (ns clj-templates.components.header.component)
 
+(def clojure-logo-src "images/clojure-logo.png")
+(def cljs-logo-src "images/cljs-logo.png")
+
 (defn header []
   [:header.header
    [:div.content
-    [:a.main-logo {:href "#/"} "clj-templates"]]])
+    [:div.main-logo "(clj-templates)"]
+    [:div.external-links
+     [:a.clojure-logo {:href "https://clojure.org/" :target "_blank"} [:img {:src clojure-logo-src}]]
+     [:a {:href "https://clojurescript.org/" :target "_blank"} [:img {:src cljs-logo-src}]]]]])

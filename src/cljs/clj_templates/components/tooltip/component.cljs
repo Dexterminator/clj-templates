5cc91e653df8cfe6d5f92389c867a8c3fcbe256a
Dexter Gramfors <dexter.gramfors@gmail.com>
2023-01-19 23:04:45 -0500

    Prevent tooltip overflows

--- src/cljs/clj_templates/components/tooltip/component.cljs
+++ src/cljs/clj_templates/components/tooltip/component.cljs
@@ -1,9 +1,16 @@
 (ns clj-templates.components.tooltip.component)
 
+(defn adjust [elem]
+  (let [x (.-x (.getBoundingClientRect elem))]
+    (when (neg? x)
+      (set! (-> elem .-style .-left) "auto")
+      (set! (-> elem .-style .-right) "0")
+      (set! (-> elem .-style .-transform) (str "translateX(" (- x) "px)")))))
+
 (defn tooltip
   ([trigger-element tooltip-content]
    [tooltip {} trigger-element tooltip-content])
   ([attrs trigger-element tooltip-content]
    [:div.tooltip attrs
     trigger-element
-    [:div.tooltip-content tooltip-content]]))
+    [:div.tooltip-content {:ref adjust} tooltip-content]]))
